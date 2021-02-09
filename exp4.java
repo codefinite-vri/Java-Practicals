0e8c3175b2adbe91d9303ca368163e8cc67223ce
Vritika Naik <37243156+codefinite-vri@users.noreply.github.com>
2021-02-09 19:05:18 +0530

    Finding the greatest number

---
+++ exp4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class greatest {
+ public static void main(String [] args)  
+ {
+     int a,b,c;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter a,b,c");
+     a=sc.nextInt();
+     b=sc.nextInt();
+     c=sc.nextInt();
+     if((a>b) && (a>c))
+     {System.out.println("largest number is"+ a);}
+     
+     if((b>c) && (b>a))
+     {System.out.println("largest number is"+ b);}
+     
+     if((c>b) && (c>a))
+     {System.out.println("largest number is"+ c);}}}
